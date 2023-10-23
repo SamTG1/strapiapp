5123d91641819447abe5fe75997cfc5a6dc80b45
tamil gameplay <sanjeevgamlcon@gmail.com>
2023-10-23 12:47:23 +0530

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: 'dsvq0t0ry',
+          api_key: '888981846165955',
+          api_secret: 'khdwUl1_enn7dGr54X62EexrO48',
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
